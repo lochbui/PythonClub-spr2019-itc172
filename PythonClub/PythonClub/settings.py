c67374f863a7156306993514128b9494b3e3180a
Loc Bui <46728521+lochbui@users.noreply.github.com>
2019-04-09 11:55:22 -0700

    urls.py-update

--- PythonClub/PythonClub/settings.py
+++ PythonClub/PythonClub/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'PythonClub.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pythonclubdb',
+        'USER' : 'postgres',
+        'PASSWORD': 'vietnam1',
+        'HOST' :'localhost',
+        'PORT' :'',
     }
 }
 
